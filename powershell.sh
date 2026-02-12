3b129dcd37ffe764404b62fbd250509195ac257a
Infiniti151 <43163551+Infiniti151@users.noreply.github.com>
2026-02-12 13:22:00 +0530

    Refactor PowerShell installation script
    Updated the script to use 'env' for bash and modified the installation logic to check the latest version from GitHub API.

--- powershell.sh
+++ powershell.sh
@@ -1,14 +1,8 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 
-install() {
+platest=$(curl -s https://api.github.com/repos/PowerShell/PowerShell/releases/latest | grep -oP '"tag_name":\s*"v\K[^"]+')
+if [[ -z $(command -v pwsh) ]] || [[ $(pwsh --version | awk '{print $2}') != "$platest" ]]; then
     sudo dnf in https://github.com/PowerShell/PowerShell/releases/download/v$platest/powershell-$platest-1.rh.x86_64.rpm
-}
-
-platest=$(git ls-remote -t https://github.com/PowerShell/PowerShell.git | awk -e '$2 ~ /v[0-9]*.[0-9]*.[0-9]*$/ {sub(/refs\/tags\/v/,"");print $2}' | sort -V | awk 'END {print}')
-[[ -z $(command -v pwsh) ]] && install && exit
-pver=$(pwsh --version | awk '{print $2}') 
-if [[ "$pver" = "$platest" ]];then
-    echo -e "Powershell is at the latest version (\033[32m$platest\033[0m)" 
 else
-    install
+    echo -e "Powershell is at the latest version (\033[32m$platest\033[0m)"
 fi
